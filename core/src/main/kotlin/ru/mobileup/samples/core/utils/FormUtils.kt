808ee43d79a8e764ed16b3c6ff3762c59f42ca87
dantrap <daniil.trapeznikov@mobileup.ru>
2025-03-26 19:30:39 +0300

    Migrate to kmm-form-validation 1.1.0, upgrade dependencies

--- core/src/main/kotlin/ru/mobileup/samples/core/utils/FormUtils.kt
+++ core/src/main/kotlin/ru/mobileup/samples/core/utils/FormUtils.kt
@@ -3,7 +3,6 @@ package ru.mobileup.samples.core.utils
 import com.arkivanov.decompose.ComponentContext
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.StateFlow
 import ru.mobileup.kmm_form_validation.control.CheckControl
 import ru.mobileup.kmm_form_validation.control.InputControl
 import ru.mobileup.kmm_form_validation.options.ImeAction
@@ -11,10 +10,8 @@ import ru.mobileup.kmm_form_validation.options.KeyboardOptions
 import ru.mobileup.kmm_form_validation.options.KeyboardType
 import ru.mobileup.kmm_form_validation.options.TextTransformation
 import ru.mobileup.kmm_form_validation.options.VisualTransformation
-import ru.mobileup.kmm_form_validation.validation.form.FormValidationResult
 import ru.mobileup.kmm_form_validation.validation.form.FormValidator
 import ru.mobileup.kmm_form_validation.validation.form.FormValidatorBuilder
-import ru.mobileup.kmm_form_validation.validation.form.dynamicValidationResult
 import ru.mobileup.kmm_form_validation.validation.form.formValidator
 import ru.mobileup.samples.core.utils.form.NumberTextTransformation
 
@@ -55,10 +52,6 @@ fun ComponentContext.CheckControl(
 fun ComponentContext.formValidator(
     buildBlock: FormValidatorBuilder.() -> Unit
 ): FormValidator = componentScope.formValidator(buildBlock)
-
-fun ComponentContext.dynamicValidationResult(
-    formValidator: FormValidator
-): StateFlow<FormValidationResult> = componentScope.dynamicValidationResult(formValidator)
 
 @OptIn(DelicateCoroutinesApi::class)
 fun fakeInputControl() = InputControl(GlobalScope)
